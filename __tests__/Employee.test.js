787ff7f18cbe804fcea21f070e688da95c8dfc78
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-05 16:00:32 -0600

    completed the Employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,4 @@
+// const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('employee gets created', () => {
@@ -6,4 +7,18 @@ test('employee gets created', () => {
 
 test('set the id', () => {
     const testValue = 100;
-})
+    const e = new Employee('Test', testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test('set the email', () => {
+    const testValue = 'test@email.com';
+    const e = new Employee('Test', 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test('getRole() return Employee', () => {
+    const testValue = 'Employee';
+    const e = new Employee('Mary', 1, 'test@email.com');
+    expect(e.getRole()).toBe(testValue);
+});

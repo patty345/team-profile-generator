5c617e4259b19847b4efbeedc2a2780687fa1e9d
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-05 15:48:45 -0600

    added employee constructor and test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('employee gets created', () => {
+    const employee = new Employee('Mary');
+});
+
+test('set the id', () => {
+    const testValue = 100;
+})
